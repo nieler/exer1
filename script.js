b46627d336718146f1e3c34381add85f12b92b37
Nielmer Camintoy <133626521+nieler@users.noreply.github.com>
2023-09-08 10:48:18 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function displaySum() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('sumButton').addEventListener("click", displaySum);
+
+function displayproduct() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('multiplyButton').addEventListener("click", displayproduct);
