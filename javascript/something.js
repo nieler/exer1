c0d8cae7d0d9f4daa038d9baa917c2420cbbd532
Nielmer Camintoy <133626521+nieler@users.noreply.github.com>
2023-09-08 09:55:27 +0800

    Create something.js

---
+++ javascript/something.js
@@ -0,0 +1,5 @@
+// id attribute has been set in all three
+
+<span id= "firstNum">2</span> <br> 
+    ...<span id= "secondNum">7</span> 
+    ...... <span id= "answer">(placeholder for the answer)</span>
