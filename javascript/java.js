79601c52edc053130c593d7ac93270c88f1dbbba
Nielmer Camintoy <133626521+nieler@users.noreply.github.com>
2023-09-08 10:21:33 +0800

    Update java.js

--- javascript/java.js
+++ javascript/java.js
@@ -7,3 +7,13 @@ function displaySum() {
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
+
+function displayproduct() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('multiplyButton').addEventListener("click", displaySum);
