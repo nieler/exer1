5ae5e57a3122158d280b21311f2aa2a64495eb98
Nielmer Camintoy <133626521+nieler@users.noreply.github.com>
2023-09-08 10:07:48 +0800

    Delete javascript/something2.js

--- javascript/something2.js
+++
@@ -1,7 +0,0 @@
-//getElementById will get all HTML elements by a specific "id" attribute
-...
-let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
-
-  let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
