66ab80bbe952069e14a4ca9b9026b04a32ff95b0
Nielmer Camintoy <133626521+nieler@users.noreply.github.com>
2023-09-08 10:08:02 +0800

    Delete javascript/something.js

--- javascript/something.js
+++
@@ -1,5 +0,0 @@
-// id attribute has been set in all three
-
-<span id= "firstNum">2</span> <br> 
-    ...<span id= "secondNum">7</span> 
-    ...... <span id= "answer">(placeholder for the answer)</span>
